9ffbdbdfef870051cd993ac9f8a608fd64ffc4ab
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2024-01-12 22:25:17 -0500

    Delete Resources directory

--- Resources/loginCheck.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", event => {
-    const app = firebase.app();
-    firebase.auth().onAuthStateChanged(user => {
-        if (!user) {
-            console.log("Redirecting to Login page")
-            window.location.href = '/Login/index.html'
-        }
-    });
-});
