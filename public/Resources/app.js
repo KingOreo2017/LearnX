1e8b1d522f8f37dc9feecbff5be3956aa08c8fd9
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2024-03-19 08:30:24 -0400

    Update app.js

--- public/Resources/app.js
+++ public/Resources/app.js
@@ -1,19 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", event => {
-    const app = firebase.app();
-
-    // Check authentication status on page load
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-
-            console.log("Redirecting to /Main Body/index.html")
-            window.location.href = '/Main Body/index.html';
-        }
-        else {
-            console.error("User not authenticated");
-        }
-    });
-});*/
-// Create popup and signs in user
 function googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
