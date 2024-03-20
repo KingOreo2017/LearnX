fddd729bfb3e98e375154d7fd454ce5240671416
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2024-03-20 09:09:24 -0400

    Update app.js

--- public/Resources/app.js
+++ public/Resources/app.js
@@ -2,18 +2,18 @@ function googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
         .then((result) => {
-            // Redirect to '/Main Body/index.html' after successful sign-in
+            const user = result.user;
+            writeUserData(user.uid, user.displayName, user.email);
+
             window.location.href = '/Main Body/index.html';
-            writeUserData(user.uid, user.dispalyName, user.email);
         })
         .catch(error => {
             console.error("Google login error:", error);
         });
-function writeUserData(userId, displayName, email) {
-    firebase.database().ref('users/' + userId).set({
-      username: displayName,
-      email: email,
-      accountType: "student"
-    });
-  }
 }
+
+function writeUserData(userId, displayName, email) {
+    var usersRef = firebase.database().ref('users');
+    var userRef = usersRef.child(displayName);
+
+    
