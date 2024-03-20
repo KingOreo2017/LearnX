3f2a44c4affd232a9386d3cd8efc030da9b3d517
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2024-03-19 21:03:36 -0400

    Create accountType.js

---
+++ public/Resources/accountType.js
@@ -0,0 +1,15 @@
+function addUser(username, email) {
+    // Get a reference to the "users" node
+    var usersRef = firebase.database().ref('users');
+
+    // Create a new child node under "users" with the username as the key
+    var userRef = usersRef.child(username);
+
+    // Set the user information (email in this case) under the user's node
+    userRef.set({
+        email: email
+    });
+}
+
+// Usage example
+addUser("KingOreo2017", "kingorreo@gmail.com");
