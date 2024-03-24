8217513f1a883a98821aa8d9f10143bccef625c8
KingOreo2017 <47092117+KingOreo2017@users.noreply.github.com>
2024-03-24 13:19:33 -0400

    Create getInfo.js

---
+++ public/Resources/getInfo.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+
+    // Check authentication status on page load
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            const photoURL = user.photoURL;
+
+            if (photoURL) {
+                // Set the profile picture
+                document.getElementById("profile-picture").src = photoURL;
+            }
+        }
+        else {
+            console.error("User not authenticated");
+        }
+    });
+});
+what does this code do?
